b2f8ac7065c8cfd2b21eb62cd9bfd72d55f439a9
michal-hynek <hynek.michal@gmail.com>
2025-04-25 08:16:49 -0700

    Chapter 7 - weather2 cargo (deserialize impl)

--- weather2/src/lib.rs
+++ weather2/src/lib.rs
@@ -1,6 +1,6 @@
-use std::env;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use reqwest::blocking::RequestBuilder;
+use serde_json::Value;
 
 #[derive(Debug, PartialEq)]
 pub struct Weather {
@@ -20,7 +20,27 @@ fn request(location: &str, api_key: &str) -> RequestBuilder {
         .query(&[("query", &location), ("access_key", &api_key)])
 }
 
+fn deserialize(json: &str) -> Result<Weather> {
+    let val: Value = serde_json::from_str(json)?;
+    let temperature = val
+        .pointer("/current/temperature")
+        .and_then(Value::as_f64)
+        .with_context(|| format!("bad response {val}"))?;
+    let summary = val
+        .pointer("/current/weather_descriptions/0")
+        .and_then(Value::as_str)
+        .with_context(|| format!("bad response {val}"))?
+        .to_string();
+
+    Ok(Weather {
+        temperature,
+        summary,
+    })
+}
+
+#[cfg(test)]
 mod tests {
+    use std::fs;
     use url::Host::Domain;
 
     use super::*;
@@ -37,15 +57,26 @@ mod tests {
         let params: Vec<(_, _)> = req.url().query_pairs().collect();
         assert_eq!(params, vec![
             ("query".into(), "Vancouver, BC".into()),
-            ("query".into(), "test-api-key".into()),
+            ("access-key".into(), "test-api-key".into()),
         ])
     }
 
     #[test]
+    fn deserialize_returns_weather_struct() {
+        let json = fs::read_to_string("tests/data/current_weather.json").unwrap();
+        let response = deserialize(&json).unwrap();
+
+        assert_eq!(response, Weather{
+            temperature: 11.1,
+            summary: "Sunny".into(),
+        });
+    }
+
+    #[test]
     fn get_weather_returns_correct_weather_for_location() {
-        let api_key = env::var("WEATHERSTACK_API_KEY").unwrap();
+        let api_key = "test-1234";
         let location = "Vancouver, BC";
-        let weather = get_weather(location, &api_key).unwrap();
+        let weather = get_weather(location, api_key).unwrap();
 
         assert_eq!(weather, Weather {
             temperature: 11.2,
