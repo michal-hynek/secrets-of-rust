4d253076918f37df06a2870e8678aa399bacedca
michal-hynek <hynek.michal@gmail.com>
2025-05-01 07:18:11 -0700

    Chapter 8 - timer cargo (added args)

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,17 +1,32 @@
 use std::process::Command;
 use std::time::Instant;
 
+use clap::Parser;
+
 use anyhow::Ok;
 
-fn main() -> anyhow::Result<()> {
-    let start = Instant::now();
+#[derive(Parser)]
+struct Args {
+    /// Program to run
+    #[arg(required = true)]
+    program: String,
 
-    let mut cmd = Command::new("cargo");
+    /// Program arguments
+    args: Vec<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let start = Instant::now();
+    let mut cmd = Command::new(args.program);
+    cmd.args(args.args);
     let output = cmd.output()?;
     let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr= String::from_utf8_lossy(&output.stderr);
 
-    println!("command output: {stdout}");
-    println!("command took: {:?}", start.elapsed());
+    print!("stdout: {stdout}");
+    println!("stderr: {stderr}");
+    println!("command took: {:.1?}", start.elapsed());
 
     Ok(())
 }
