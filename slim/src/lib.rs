d70fc09722ddb62c7b3f4ab4157ff4886451d920
michal-hynek <hynek.michal@gmail.com>
2025-05-04 10:45:45 -0700

    Chapter 8 - slim cargo (manifests impl)

--- slim/src/lib.rs
+++ slim/src/lib.rs
@@ -1,15 +1,51 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
+use walkdir::WalkDir;
 
 pub fn slim(path: impl AsRef<Path>) -> Result<String> {
     let mut output = String::new();
 
     for target in manifests(path)? {
-        let mut cmd = cargo_clean_cmd(&target);
-        let cmd_output = cmd.output()?;
-        output.push_str(&summary(target, &cmd_output));
+        //let mut cmd = cargo_clean_cmd(&target);
+        //let cmd_output = cmd.output()?;
+        //output.push_str(&summary(target, &cmd_output));
     }
 
     Ok(output)
+}
+
+fn manifests(path: impl AsRef<Path>) -> Result<Vec<PathBuf>> {
+    let mut targets = Vec::new();
+
+    for entry in WalkDir::new(path) {
+        let entry = entry?;
+
+        if entry.file_name() == "Cargo.toml" {
+            targets.push(entry.path().to_path_buf());
+        }
+    }
+
+    Ok(targets)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn manifests_returns_cargo_toml_paths() {
+        let mut tomls = manifests("tests").unwrap();
+        tomls.sort();
+
+        assert_eq!(
+            tomls,
+            vec![
+                PathBuf::from("tests/data/proj_1/Cargo.toml"),
+                PathBuf::from("tests/data/proj_2/Cargo.toml"),
+                PathBuf::from("tests/data/proj_3/Cargo.toml"),
+            ],
+        );
+    }
+
 }
