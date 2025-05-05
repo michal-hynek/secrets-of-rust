224a36041f8216de311c70d0b2b61042d9ef4466
michal-hynek <hynek.michal@gmail.com>
2025-05-05 07:28:43 -0700

    Chapter 8 - slim cargo (cargo_clean_cmd impl)

--- slim/src/lib.rs
+++ slim/src/lib.rs
@@ -1,4 +1,5 @@
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use anyhow::Result;
 use walkdir::WalkDir;
@@ -7,8 +8,8 @@ pub fn slim(path: impl AsRef<Path>) -> Result<String> {
     let mut output = String::new();
 
     for target in manifests(path)? {
-        //let mut cmd = cargo_clean_cmd(&target);
-        //let cmd_output = cmd.output()?;
+        let mut cmd = cargo_clean_cmd(&target)?;
+        let cmd_output = cmd.output()?;
         //output.push_str(&summary(target, &cmd_output));
     }
 
@@ -29,6 +30,17 @@ fn manifests(path: impl AsRef<Path>) -> Result<Vec<PathBuf>> {
     Ok(targets)
 }
 
+fn cargo_clean_cmd(path: impl AsRef<Path>) -> Result<Command> {
+    let mut cmd = Command::new("cargo");
+    cmd.args([
+        "clean",
+        "--manifest-path",
+        &path.as_ref().to_string_lossy(),
+    ]);
+    
+    Ok(cmd)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,6 +57,17 @@ mod tests {
                 PathBuf::from("tests/data/proj_2/Cargo.toml"),
                 PathBuf::from("tests/data/proj_3/Cargo.toml"),
             ],
+        );
+    }
+
+    #[test]
+    fn cargo_clean_cmd_returns_correct_command() {
+        let cmd = cargo_clean_cmd("tests/data/proj_1/Cargo.toml").unwrap();
+
+        assert_eq!(cmd.get_program(), "cargo");
+        assert_eq!(
+            cmd.get_args().collect::<Vec<_>>(),
+            ["clean", "--manifest-path", "tests/data/proj_1/Cargo.toml"],
         );
     }
 
